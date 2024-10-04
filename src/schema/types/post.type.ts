a5c9ceaaef8c5a79be45f64cd519784a7af927e0
Ethernaljoz <amoussoujoel500@gmail.com>
2024-10-04 23:57:01 +0000

    project query probleme fixed

--- src/schema/types/post.type.ts
+++ src/schema/types/post.type.ts
@@ -1,6 +1,5 @@
 import { GraphQLObjectType, GraphQLString, GraphQLID } from "graphql";
 import { UserType } from "./user.type";
-import { resolve } from "path";
 import { UserModel } from "../../models/user.model";
 
 export const PostType = new GraphQLObjectType({
@@ -9,14 +8,10 @@ export const PostType = new GraphQLObjectType({
         _id: {type: GraphQLID},
         title:{type: GraphQLString},
         content:{type: GraphQLString},
-        userID:{
+        user:{
             type: UserType,
             async resolve(parent, args){
-                const user = await UserModel.findById(parent._id)
-                if(!user){
-                    throw new Error("user not found")
-                }
-                return user
+                return await UserModel.findById(parent.userID)
             }
 
         },
